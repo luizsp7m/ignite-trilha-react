1dc469302b9defdb91c371cb01f4be6158e65ea8
Luiz <luizoliveira2808@gmail.com>
2021-12-11 00:49:22 -0300

    Componente Modal criado

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
-import { Dashboard } from "./components/Dashboard";
+import { Fragment, useState } from "react";
 
+import Modal from 'react-modal';
+
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root');
+
 export function App() {
-  const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionOpen] = useState(false);
 
   function handleOpenNewTransactionModal() {
-    setIsNewTransactionsModalOpen(true);
+    setIsNewTransactionOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionOpen(false);
   }
 
   return (
-    <>
+    <Fragment>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
-    </>
+    </Fragment>
   );
 }
